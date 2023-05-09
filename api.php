4d98ae43b006915a5a91d30da6b826ea29fe9351
Guilherme Augusto Fernandes <69806054+guigutox@users.noreply.github.com>
2023-05-09 12:11:09 -0300

    Criação do get
    Criação do método get e requisição por nome na url

--- api.php
+++ api.php
@@ -21,8 +21,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $sql = "INSERT INTO pessoas (nome, idade) VALUES ('$nome', $idade)";
     mysqli_query($conn, $sql);
     $id = mysqli_insert_id($conn);
-    echo json_encode(array('id' => $id));
+    echo json_encode(array('id' => $id, 'nome' => $nome, 'idade' => $idade));
 }
+
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+
+    $varID = $_GET['id'];
+    $varNome = $_GET['nome'];
+
+    $sql = "SELECT * FROM pessoas";
+    if($varID){
+        $sql .= " WHERE id = $varID";
+    }else if($varNome){
+        $sql .= " Where nome = $varNome";
+    }
+
+    
+    $result = mysqli_query($conn, $sql);
+    $pessoas = array();
+    while ($row = mysqli_fetch_assoc($result)){
+        $pessoas[] = $row;
+    }
+    echo json_encode($pessoas);
+}
+
 
 
 ?>
